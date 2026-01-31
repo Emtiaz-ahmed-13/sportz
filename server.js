80b5a80d8ba78c9fad8c5ece0c12f46af92a68c8
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-02-01 01:22:43 +0600

    feat: Add API endpoint for creating matches, implement match status logic, and configure database SSL.

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const matchRouter = require('./src/routes/matches');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // JSON middleware
 app.use(express.json());
 
 // Root GET route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Express.js server!' });
+  res.json({ message: 'Welcome to the Sportz API!' });
 });
+
+// Routes
+app.use('/api/matches', matchRouter);
 
 // Start server
 app.listen(PORT, () => {
